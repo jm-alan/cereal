3559a68e8897da345ce7959cd4a432f01c1d013b
JM <j.maxwell.alan@gmail.com>
2024-05-06 23:01:55 -0400

    add xtra comparison to main run

--- Sources/CerealClient/main.swift
+++ Sources/CerealClient/main.swift
@@ -1,4 +1,5 @@
 import Cereal
+import ExtrasJSON
 import Foundation
 
 let a = 17
@@ -8,61 +9,57 @@ let b = 25
 struct TryMe {
     var someVar: Int = 0
 }
+
 let tried = TryMe()
 
-let encoder = JSONEncoder()
+let codableEncoder = JSONEncoder()
+let xtraEncoder = XJSONEncoder()
 
 let info = KgInfo()
 
 let runs = 10_000_000
 
-var codableResult = try! String(data: encoder.encode(info), encoding: .utf8)!
+var codableResult = try! String(data: codableEncoder.encode(info), encoding: .utf8)!
+var xtraResult = try! String(bytes: xtraEncoder.encode(info), encoding: .utf8)!
 var cerealResult = info.serialize()
 
-//let codableStart = Date().timeIntervalSince1970
-//for _ in 0..<runs {
-//    codableResult = try! String(data: encoder.encode(info), encoding: .utf8)
-//}
-//let codableEnd = Date().timeIntervalSince1970
-//let codableTime = codableEnd - codableStart
-//
-//let cerealStart = Date().timeIntervalSince1970
-//for _ in 0..<runs {
-//    cerealResult = info.serialize()
-//}
-//let cerealEnd = Date().timeIntervalSince1970
-//let cerealTime = cerealEnd - cerealStart
-//
-//let codableIPS = Double(runs) / codableTime
-//let cerealIPS = Double(runs) / cerealTime
-//
-//func roundReadable(_ dub: Double) -> Double {
-//    Double(round(dub * 10_000)) / 10_000
-//}
-
-//print("Codable took \(codableTime)s or ~\(roundReadable(codableIPS)) iterations per second")
-//print("Cereal took \(cerealTime)s or ~\(roundReadable(cerealIPS)) iterations per second")
-//print("Cereal performed at ~\(Double(round((cerealIPS / codableIPS) * 10_000)) / 100.0)% of Codable")
-
-let isolatedEscapeTest = String(cerealResult)
-
-let escapeEncoder = JSONEncoder()
-let codableEscapeStart = Date().timeIntervalSince1970
-for idx in 0..<runs {
-    codableResult = try! String(data: escapeEncoder.encode(isolatedEscapeTest), encoding: .utf8)!
+let codableStart = Date().timeIntervalSince1970
+for _ in 0 ..< runs {
+    codableResult = try! String(data: codableEncoder.encode(info), encoding: .utf8)!
 }
-let codableEscapeEnd = Date().timeIntervalSince1970
-let codableEscapeTime = codableEscapeEnd - codableEscapeStart
 
-let cerealEscapeStart = Date().timeIntervalSince1970
-for idx in 0..<runs {
-    cerealResult = Parse.escape(string: isolatedEscapeTest)
+let codableEnd = Date().timeIntervalSince1970
+let codableTime = codableEnd - codableStart
+
+let xtraStart = Date().timeIntervalSince1970
+for _ in 0 ..< runs {
+    xtraResult = try! String(bytes: xtraEncoder.encode(info), encoding: .utf8)!
 }
-let cerealEscapeEnd = Date().timeIntervalSince1970
-let cerealEscapeTime = cerealEscapeEnd - cerealEscapeStart
 
-let codableEscapeIPS = Double(runs) / codableEscapeTime
-let cerealEscapeIPS = Double(runs) / cerealEscapeTime
+let xtraEnd = Date().timeIntervalSince1970
+let xtraTime = xtraEnd - xtraStart
 
-print("Codable took \(codableEscapeTime) | ~\(codableEscapeIPS) iterations per second | ~\(.init(codableResult.utf8.count) * codableEscapeIPS / 1_000_000.0) MB/s")
-print("Cereal took \(cerealEscapeTime) | ~\(cerealEscapeIPS) iterations per second | ~\(.init(cerealResult.utf8.count) * cerealEscapeIPS / 1_000_000.0) MB/s")
+let cerealStart = Date().timeIntervalSince1970
+for _ in 0 ..< runs {
+    cerealResult = info.serialize()
+}
+
+let cerealEnd = Date().timeIntervalSince1970
+let cerealTime = cerealEnd - cerealStart
+
+let runsAsDouble: Double = .init(runs)
+
+let codableIPS = runsAsDouble / codableTime
+let xtraIPS = runsAsDouble / xtraTime
+let cerealIPS = runsAsDouble / cerealTime
+
+func roundReadable(_ dub: Double) -> Double {
+    Double(round(dub * 10000)) / 10000
+}
+
+print("Codable took \(codableTime)s | ~\(roundReadable(codableIPS)) iterations per second | ~\(.init(codableResult.utf8.count) * codableIPS / 1_000_000.0) MB/s")
+print("Xtra took \(xtraTime)s | ~\(roundReadable(xtraIPS)) iterations per second | ~\(.init(xtraResult.utf8.count) * xtraIPS / 1_000_000.0) MB/s")
+print("Cereal took \(cerealTime)s | ~\(roundReadable(cerealIPS)) iterations per second | ~\(.init(cerealResult.utf8.count) * cerealIPS / 1_000_000.0) MB/s")
+print("Cereal performed at ~\(Double(round((cerealIPS / codableIPS) * 10000)) / 100.0)% of Codable")
+print("Xtra performed at ~\(Double(round((xtraIPS / codableIPS) * 10000)) / 100.0)% of Codable")
+print("Cereal performed at ~\(Double(round((cerealIPS / xtraIPS) * 10000)) / 100.0)% of Xtra")
